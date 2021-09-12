342b3190b5495f83aed4f72087dfc3e759e9febc
musfarkani <musfarkani@gmail.com>
2021-09-13 00:10:23 +0530

    main

--- src/App.js
+++ src/App.js
@@ -2,30 +2,49 @@ import './App.css'
 import {useState} from 'react'
 function App() {
   const [toDos,setToDos] = useState([])
-  const [ToDo,setToDo] = useState('')
+  const [toDo,setToDo] = useState('')
     return (
     <div className="app">
       <div className="mainHeading">
-        <h1>ToDo List</h1>
+        <h1>To Do Task</h1>
       </div>
       <div className="subHeading">
         <br />
-        <h2>whoop, it's Wednesday 🌝 ☕ </h2>
+        <h2>Hi ! Have a Pleasant day 🌝 ☕ </h2>
       </div>
-      <div className="input">
-        <input value={toDos} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+      <div className="input">  
+        <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>setToDos([...toDos,{id:Date.now() ,text: toDo,status:false}])} className="fas fa-plus"></i>
       </div>
       <div className="toDos">
-        <div className="toDo">
-          <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>React Todo Note</p>
-          </div>
+        {toDos.map((obj)=>{
+          
+          return ( <div className="toDo">
+          <div className="left"> 
+            <input onChange={(e)=>{
+            console.log(e.target.checked)
+            console.log(obj)
+            setToDos(toDos.filter(obj2=>{
+              if(obj2.id===obj.id){
+                obj2.status=e.target.checked
+              }
+              return obj2
+            }))
+            }}
+            value={obj.status} type="checkbox" name="" id="" />
+            <p>{obj.text}</p>
+             </div>
           <div className="right">
             <i className="fas fa-times"></i>
           </div>
-        </div>
+        </div>)
+        })}
+        {toDos.map((obj)=>{
+          if(obj.status){
+          return(<h1>{obj.text}</h1>)
+          }
+          return null
+        })}
       </div>
     </div>
   );
